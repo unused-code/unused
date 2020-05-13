1f0ea1d613f25da980ff70768442a8e569c89dd4
Joshua Clayton <joshua.clayton@gmail.com>
2020-05-12 22:16:22 -0400

    `TagsReader` uses `git rev-parse --git-dir` to determine `.git` path
    What?
    =====
    
    Rather than assume that reading tags will occur within the current
    directory's `.git` directory, this uses `git` itself to determine the
    application root.

--- crates/read_ctags/src/tags_reader.rs
+++ crates/read_ctags/src/tags_reader.rs
@@ -2,11 +2,13 @@ use super::{CtagItem, CtagsParseError};
 use std::collections::HashSet;
 use std::convert::From;
 use std::default::Default;
+use std::env::current_dir;
 use std::fmt::{Display, Formatter};
 use std::fs;
 use std::io;
 use std::io::Error;
 use std::path::PathBuf;
+use std::process::Command;
 
 /// TagsReader provides a mechanism for attempting to read multiple ctags files until the first is
 /// found
@@ -48,15 +50,48 @@ impl Display for ReadCtagsError {
     }
 }
 
+fn git_path() -> Option<PathBuf> {
+    match Command::new("git")
+        .arg("rev-parse")
+        .arg("--git-dir")
+        .output()
+    {
+        Ok(o) => {
+            if o.status.success() {
+                std::str::from_utf8(&o.stdout)
+                    .ok()
+                    .map(|k| PathBuf::from(k.trim_end()))
+            } else {
+                None
+            }
+        }
+        Err(_) => None,
+    }
+}
+
+fn cwd_tags_paths() -> Vec<PathBuf> {
+    vec![PathBuf::from("tags"), PathBuf::from("tmp/tags")]
+}
+
 impl Default for TagsReader {
     fn default() -> Self {
-        TagsReader {
-            filenames: vec![
-                PathBuf::from(".git/tags"),
-                PathBuf::from("tags"),
-                PathBuf::from("tmp/tags"),
-            ],
+        let mut filenames = vec![];
+
+        if let Ok(current_dir) = current_dir() {
+            if let Some(app_git_path) = git_path() {
+                if current_dir == app_git_path {
+                    filenames.push(app_git_path.join("tags"));
+                    filenames.extend(cwd_tags_paths());
+                } else {
+                    filenames.extend(cwd_tags_paths());
+                    filenames.push(app_git_path.join("tags"));
+                    filenames.push(app_git_path.join("../tags"));
+                    filenames.push(app_git_path.join("../tmp/tags"));
+                }
+            }
         }
+
+        TagsReader { filenames }
     }
 }
 
