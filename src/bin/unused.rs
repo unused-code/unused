e13c5ce056818ee8cd39b8f044ee4b1908024bd5
Joshua Clayton <joshua.clayton@gmail.com>
2021-06-11 07:02:47 -0400

    Make mimalloc optional
    What?
    =====
    
    mimalloc significantly reduces execution time in local benchmarking, but
    may be causing problems with ARM architectures (specifically the MBP
    M1s).
    
    This shifts mimalloc from a hard dependency to a feature flag which can
    be configured during install.

--- src/bin/unused.rs
+++ src/bin/unused.rs
@@ -1,4 +1,4 @@
-#[cfg(all(unix, not(target_env = "musl")))]
+#[cfg(all(feature = "mimalloc", unix, not(target_env = "musl")))]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
